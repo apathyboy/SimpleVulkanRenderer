e2d4471c910aca541685b408f56c563ff6d2ff35
eb <eb@2bros.dev>
2020-12-02 03:26:43 -0600

    create instance

--- src/vulkan_simple_context.cpp
+++ src/vulkan_simple_context.cpp
@@ -3,6 +3,23 @@
 
 namespace svr {
 
-vulkan_simple_context::vulkan_simple_context() {}
+vulkan_simple_context::vulkan_simple_context()
+{
+    if (vk::enumerateInstanceVersion() < min_vulkan_api_version_) {
+        throw std::runtime_error{"Min Vulkan API version not found"};
+    }
+
+    // Create context
+    create_instance();
+}
+
+void vulkan_simple_context::create_instance()
+{
+    vk::ApplicationInfo application_info = {nullptr, 0, nullptr, 0, min_vulkan_api_version_};
+    vk::InstanceCreateInfo create_info =
+        {{}, &application_info, enabled_layers_, enabled_instance_extensions_};
+
+    instance_ = vk::createInstanceUnique(create_info);
+}
 
 } // namespace svr
