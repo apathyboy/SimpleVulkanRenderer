0a19dec61d43e002981ae864f3289ff37ba3f29d
eb <eb@2bros.dev>
2020-12-14 10:25:05 -0600

    add checks for layers and extensions

--- src/vulkan_simple_context.cpp
+++ src/vulkan_simple_context.cpp
@@ -3,10 +3,48 @@
 
 namespace svr {
 
+bool has_required_layers(
+    const std::vector<vk::LayerProperties>& available,
+    const std::vector<const char*>&         required)
+{
+    return std::ranges::all_of(required, [&available](const auto& name) {
+        return std::ranges::any_of(available, [&name](const auto& p) {
+            return strcmp(p.layerName, name) == 0;
+        });
+    });
+}
+
+bool has_required_extensions(
+    const std::vector<vk::ExtensionProperties>& available,
+    const std::vector<const char*>&             required)
+{
+    return std::ranges::all_of(required, [&available](const auto& name) {
+        return std::ranges::any_of(available, [&name](const auto& p) {
+            return strcmp(p.extensionName, name) == 0;
+        });
+    });
+}
+
 vulkan_simple_context::vulkan_simple_context()
 {
     if (vk::enumerateInstanceVersion() < min_vulkan_api_version_) {
         throw std::runtime_error{"Min Vulkan API version not found"};
+    }
+
+#ifndef NDEBUG
+    enabled_layers_.push_back("VK_LAYER_KHRONOS_validation");
+#endif
+
+    // Verify required layers are available
+    if (!has_required_layers(vk::enumerateInstanceLayerProperties(), enabled_layers_)) {
+        throw std::runtime_error{"Required layers not found"};
+    }
+
+    // Verify required extensions are available
+    if (!has_required_extensions(
+            vk::enumerateInstanceExtensionProperties(),
+            enabled_instance_extensions_)) {
+        throw std::runtime_error{"Required extensions not found"};
     }
 
     // Create context
